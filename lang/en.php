6f6b154493b677e551e49d69531b38298e02d60e
Ömer ATABER <67821076+OmerAti@users.noreply.github.com>
2024-09-19 14:53:59 +0300

    Add files via upload

---
+++ lang/en.php
@@ -0,0 +1,10 @@
+<?php 
+return [
+    'meta'     => [
+        'name'         => "All Comments Custom Page Plugin",
+        'descriptions' => "Adds a custom page to your site.",
+    ],
+    'YorumWiseCP_yol_name' => "Theme Path",
+    'YorumWiseCP_yol_description' => "Please enter the theme path",
+    'success1' => "Settings saved successfully.",
+];
---
+++ lang/tr.php
@@ -0,0 +1,10 @@
+<?php 
+return [
+    'meta'     => [
+        'name'         => "Tüm Yorumlar Özel Sayfa Eklentisi",
+        'descriptions' => "Sitenize Özel Sayfa Ekler.",
+    ],
+    'YorumWiseCP_yol_name' => "Tema Yolu",
+    'YorumWiseCP_yol_description' => "Lütfen tema yolunu girin",
+    'success1' => "Ayarlar başarıyla kaydedildi.",
+];
